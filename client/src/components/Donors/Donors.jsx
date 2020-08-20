3fb5c3ca9d4c91f16a68df55fdacc00557fa6bbd
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-20 15:36:18 -0400

    component placeholders

---
+++ client/src/components/Donors/Donors.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Donors.css'
+
+function Donors() {
+  return (
+    <div className="donors">
+      <h1>Donors</h1>
+    </div>
+  )
+}
+
+export default Donors
---
+++ client/src/components/shared/HeaderNav/HeaderNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeaderNav.css'
+
+function HeaderNav() {
+  return (
+    <div className="header-nav">
+      <h1>HEADER/NAV</h1>
+    </div>
+  )
+}
+
+export default HeaderNav
