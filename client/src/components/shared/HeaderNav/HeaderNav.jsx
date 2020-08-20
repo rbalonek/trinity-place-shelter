27d911d038a13e91c87099234325abba6944b0e0
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-20 19:43:35 -0400

    mvp style

--- client/src/components/shared/HeaderNav/HeaderNav.jsx
+++ client/src/components/shared/HeaderNav/HeaderNav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './HeaderNav.css'
+import Logo from '../../../assets/Logo/Trinity-Logo.png'
 
 function HeaderNav() {
   return (
     <div className="header-nav">
-      <h1>HEADER/NAV</h1>
+      <img src={Logo} alt="trinity place shelter logo" />
+      <nav>
+        <ul className="header-nav__container">
+          <li>OUR STORY</li>
+          <li>VOLUNTEER</li>
+          <li>RESOURCES</li>
+          <li>FAQ</li>
+        </ul>
+        <button className="header-nav__donateButton">DONATE</button>
+      </nav>
     </div>
   )
 }
