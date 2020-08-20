e3383fda5a25b8405142a5c586996cde7a37f6c9
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-20 14:53:57 -0400

    fix route

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -3,7 +3,7 @@ const controllers = require('../controllers/volunteers')
 
 const router = Router()
 
-router.get('/volunteers', controllers.getVolunteer)
+router.get('/volunteers', controllers.getVolunteers)
 router.get('/volunteers/:id', controllers.getVolunteer)
 router.post('/volunteers', controllers.createVolunteer)
 router.put('/volunteers/:id', controllers.updateVolunteer)
