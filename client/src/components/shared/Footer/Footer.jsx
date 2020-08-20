2a312c811d3006e26ea23cf2a3186b805983391d
Robert Balonek <robertbalonek@gmail.com>
2020-08-20 17:22:55 -0400

    Correct Size and Position

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import './Footer.css'
+// @ts-nocheck
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="container">
-      <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=500px&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width="340"
-        height="300px"
-        className="iFrame"
-        scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        allow="encrypted-media">
-      </iframe>
+        <iFrame
+          className="iFrame"
+          height="400px"
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftrinityplaceshelter%2F&tabs=timeline&width=340&height=500px&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
