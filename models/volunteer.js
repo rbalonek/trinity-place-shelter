3558496586418cb02853dacb5404ad621f0bf005
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-20 14:10:05 -0400

    Schema

---
+++ models/volunteer.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Volunteer = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    secondAddress: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('volunteers', Volunteer)
---
+++ routes/volunteers.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+const controllers = require('../controllers/volunteers')
+
+const router = Router()
+
+router.get('/volunteers', controllers.getVolunteer)
+router.get('/volunteers/:id', controllers.getVolunteer)
+router.post('/volunteers', controllers.createVolunteer)
+router.put('/volunteers/:id', controllers.updateVolunteer)
+router.delete('/volunteers/:id', controllers.deleteVolunteer)
+
+module.exports = router
