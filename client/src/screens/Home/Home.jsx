d078ef0de376b44f7bda084f6b8d737a50b2a86c
Robert Balonek <robertbalonek@gmail.com>
2020-08-20 16:38:43 -0400

    Setup Footer

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Home.css'
 
+//Components
+import Footer from "../../components/shared/Footer/Footer"
+
 function Home() {
   return (
     <div className="home">
       <h1>Welcome HOME</h1>
+      <Footer />
     </div>
   )
 }
