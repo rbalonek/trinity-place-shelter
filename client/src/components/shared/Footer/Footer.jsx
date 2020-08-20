22436958ce6dc3914046a8f5740581bf0d7f773a
Robert Balonek <robertbalonek@gmail.com>
2020-08-20 17:06:38 -0400

    Got iFrame

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -4,7 +4,18 @@ import './Footer.css'
 function Footer() {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <div className="container">
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=500px&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="340"
+        height="300px"
+        className="iFrame"
+        scrolling="no"
+        frameborder="0"
+        allowTransparency="true"
+        allow="encrypted-media">
+      </iframe>
+      </div>
     </div>
   )
 }
