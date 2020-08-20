03325e9535988057fc75e1a7c9161f8ac24079b2
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-20 15:14:59 -0400

    screen wireframes

---
+++ client/src/screens/DonateForm/DonateForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DonateForm.css'
+
+function DonateForm() {
+  return (
+    <div className="donate-form">
+      <h1>Welcome to DonateForm</h1>
+    </div>
+  )
+}
+
+export default DonateForm
---
+++ client/src/screens/Volunteer/Volunteer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Volunteer.css'
+
+function Volunteer() {
+  return (
+    <div className="volunteer">
+      <h1>Welcome to Volunteer</h1>
+    </div>
+  )
+}
+
+export default Volunteer
