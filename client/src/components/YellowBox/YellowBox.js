59341d64fa3100e2623bc6883dcf97ef5002a376
Robert Balonek <robertbalonek@gmail.com>
2020-08-21 14:59:54 -0400

    Centered Img

---
+++ client/src/components/YellowBox/YellowBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./YellowBox.css"
+
+export default function YellowBox() {
+  return (
+    <div className="yelow-box">
+      <img className="hand-heart" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945995/Trinity-Place-Shelter/HomePage/Hands_Image_hyeuaq.png" alt=""/>
+    </div>
+  )
+}
--- client/src/screens/VolunteerFormScreen/VolunteerFormScreen.jsx
+++ client/src/screens/VolunteerFormScreen/VolunteerFormScreen.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import VolunteerForm from '../../components/VolunteerForm/VolunteerForm'
 import './VolunteerFormScreen.css'
 
+//Component
+import YellowBox from "../../components/YellowBox/YellowBox.js"
+
 function VolunteerFormScreen() {
   return (
     <div className="volunteer-form">
       
       <VolunteerForm />
+      <YellowBox />
     </div>
   )
 }
