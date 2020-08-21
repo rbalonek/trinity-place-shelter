768c2b5a985e05c72b746465f0fe45a4491b2c94
Robert Balonek <robertbalonek@gmail.com>
2020-08-20 20:48:44 -0400

    Positioning finished

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -5,33 +5,54 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-    <div >
-      <div className="container">
-        <div className="container-left">
-          <div className="footer-nav-links">
-            <p>Home</p>
-            <p>Our Story</p>
-            <p>Volunteer</p>
-            <p>Resources</p>
-            <p>Faq</p>
-            <p>Donate</p>
+      <div>
+        <div className="container">
+          
+          <div className="container-left">
+            <div className="footer-nav-links">
+              <p>Home</p> <br/>
+              <p>Our Story</p> <br/>
+              <p>Volunteer</p><br/>
+              <p>Resources</p><br/>
+              <p>FAQ</p><br/>
+              <p>Donate</p><br/>
+            </div>
+            <div className="footer-501-info">
+            <p>Trinity Community Connection</p>
+            <p>is a 501(c)(3) Organization</p><br/><br/>
+            <p>164 West 100th</p>
+            <p>New York, New York 10025</p><br/><br/>
+            <p>646-580-7045</p><br/>
+            <p>Contact Us</p>
+            </div>
+            <div className="footer-payment-methods">
+            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/22_cjqquj.png" alt=""/>
+            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/14_q0evfg.png" alt="" />
+            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/5_ydtq4z.png" alt="" />
+           
+            </div>
+            <div className="footer-payment-methods-bottom">
+            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/2_a7yimq.png" alt="" />
+            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/1_pza0ul.png" alt="" />
+            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/3_inrnou.png" alt=""/>
+            </div>
+          </div>
+
+          <div className="container-right"></div>
+          <div className="iframe-div">
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftrinityplaceshelter%2F&tabs=timeline&width=200&height=300&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+              width="200"
+              height="300"
+              scrolling="no"
+              frameborder="0"
+              allowTransparency="true"
+              allow="encrypted-media">
+            </iframe>
           </div>
         </div>
-
-        <div className="container-right"></div>
-        <div className="iframe-div">
-          <iframe
-            className="iFrame"
-            height="300px"
-            width="340px"
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftrinityplaceshelter%2F&tabs=timeline&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-            frameborder="0"
-            allowTransparency="true"
-          />
-        </div>
       </div>
-      </div>
-      </footer>
+    </footer>
   );
 }
 
--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -3,16 +3,11 @@ import "./Home.css";
 
 //Components
 import Footer from "../../components/shared/Footer/Footer";
-import ExtraSpace from "../../components/shared/Footer/ExtraSpace.js";
 
 function Home() {
   return (
     <div className="home">
       <h1>Welcome HOME</h1>
-      <ExtraSpace>
-      <h1>LALA</h1>
-      </ExtraSpace>
-
       <Footer />
     </div>
   );
