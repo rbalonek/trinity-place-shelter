5c32453d7e415a80a840e9c9cc378e25c23207dc
Robert Balonek <robertbalonek@gmail.com>
2020-08-20 20:56:14 -0400

    All non-dead links functional

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -1,40 +1,80 @@
 // @ts-nocheck
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer">
       <div>
         <div className="container">
-          
           <div className="container-left">
             <div className="footer-nav-links">
-              <p>Home</p> <br/>
-              <p>Our Story</p> <br/>
-              <p>Volunteer</p><br/>
-              <p>Resources</p><br/>
-              <p>FAQ</p><br/>
-              <p>Donate</p><br/>
+              <Link to="/">
+                <p>Home</p>
+              </Link>{" "}
+              <br />
+              <p>Our Story</p>
+              <br />
+              <Link to="/volunteer">
+                <p>Volunteer</p>
+                <br />
+              </Link>
+              <p>Resources</p>
+              <br />
+              <p>FAQ</p>
+              <br />
+              <Link to="/donate">
+                <p>Donate</p>
+                <br />
+              </Link>
             </div>
             <div className="footer-501-info">
-            <p>Trinity Community Connection</p>
-            <p>is a 501(c)(3) Organization</p><br/><br/>
-            <p>164 West 100th</p>
-            <p>New York, New York 10025</p><br/><br/>
-            <p>646-580-7045</p><br/>
-            <p>Contact Us</p>
+              <p>Trinity Community Connection</p>
+              <p>is a 501(c)(3) Organization</p>
+              <br />
+              <br />
+              <p>164 West 100th</p>
+              <p>New York, New York 10025</p>
+              <br />
+              <br />
+              <p>646-580-7045</p>
+              <br />
+              <p>Contact Us</p>
             </div>
             <div className="footer-payment-methods">
-            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/22_cjqquj.png" alt=""/>
-            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/14_q0evfg.png" alt="" />
-            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/5_ydtq4z.png" alt="" />
-           
+              <img
+                className="footer-card"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/22_cjqquj.png"
+                alt=""
+              />
+              <img
+                className="footer-card"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/14_q0evfg.png"
+                alt=""
+              />
+              <img
+                className="footer-card"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/5_ydtq4z.png"
+                alt=""
+              />
             </div>
             <div className="footer-payment-methods-bottom">
-            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/2_a7yimq.png" alt="" />
-            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/1_pza0ul.png" alt="" />
-            <img className="footer-card" src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/3_inrnou.png" alt=""/>
+              <img
+                className="footer-card"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/2_a7yimq.png"
+                alt=""
+              />
+              <img
+                className="footer-card"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/1_pza0ul.png"
+                alt=""
+              />
+              <img
+                className="footer-card"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/3_inrnou.png"
+                alt=""
+              />
             </div>
           </div>
 
@@ -47,8 +87,8 @@ function Footer() {
               scrolling="no"
               frameborder="0"
               allowTransparency="true"
-              allow="encrypted-media">
-            </iframe>
+              allow="encrypted-media"
+            ></iframe>
           </div>
         </div>
       </div>
