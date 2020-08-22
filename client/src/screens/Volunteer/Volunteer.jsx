915f2b46c7ae3e33ab014be23b31b5dd818a4f20
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-22 12:09:38 -0400

    sketch content box

--- client/src/screens/Volunteer/Volunteer.jsx
+++ client/src/screens/Volunteer/Volunteer.jsx
@@ -7,7 +7,18 @@ import HomeIconBox from '../../components/HomeIconBox/HomeIconBox.js'
 function Volunteer() {
   return (
     <div className="volunteer">
-      <h1>Welcome to Volunteer</h1>
+      <div className="volunteer__banner">
+        <div className="volunteer__content">
+          <h1>DIVIDER</h1>
+        </div>
+        <div className="volunteer__carousel">
+          <h1>EVERYONE DESERVES...</h1>
+          <h1>A WARM BED</h1>
+          <h1>A SAFE SPACE</h1>
+          <h1>A ROOF OVERHEAD</h1>
+          <h1>LOVE & ACCEPTANCE</h1>
+        </div>
+      </div>
       <HomeIconBox />
     </div>
   )
