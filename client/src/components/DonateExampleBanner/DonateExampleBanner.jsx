3e17d29f847f46f41758bfeb906fe592a4c4e9df
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-24 11:14:35 -0400

    moar style

--- client/src/components/DonateExampleBanner/DonateExampleBanner.jsx
+++ client/src/components/DonateExampleBanner/DonateExampleBanner.jsx
@@ -5,6 +5,11 @@ function DonateExampleBanner() {
   return (
     <div className="donateExampleBanner">
       <h1>HERE ARE EXAMPLES OF HOW YOUR GIFT WILL MAKE AN IMPACT</h1>
+      <div className="donateExampleBanner__circles">
+        <div className="donateExampleBanner__circle --orange "></div>
+        <div className="donateExampleBanner__circle --red"></div>
+        <div className="donateExampleBanner__circle --purple"></div>
+      </div>
     </div>
   )
 }
