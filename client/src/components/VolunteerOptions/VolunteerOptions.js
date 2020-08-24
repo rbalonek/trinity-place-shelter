38a7c8f74cc8a1642c22110c50a9f13fb635fc54
Robert Balonek <robertbalonek@gmail.com>
2020-08-24 09:51:09 -0400

    3 images

---
+++ client/src/components/VolunteerOptions/VolunteerOptions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./VolunteerOptions.css"
+
+export default function VolunteerOptions() {
+  return (
+    <div className="volunteer-options-container">
+      <div className="left-div"></div>
+      <div className="middle-div"></div>
+      <div className="right-div"></div>
+    </div>
+  )
+}
--- client/src/screens/Volunteer/Volunteer.jsx
+++ client/src/screens/Volunteer/Volunteer.jsx
@@ -3,11 +3,13 @@ import './Volunteer.css'
 
 //Components
 import HomeIconBox from "../../components/HomeIconBox/HomeIconBox.js"
+import VolunteerOptions from "../../components/VolunteerOptions/VolunteerOptions.js"
 
 function Volunteer() {
   return (
     <div className="volunteer">
       <h1>Welcome to Volunteer</h1>
+      <VolunteerOptions />
       <HomeIconBox />
     </div>
   )
