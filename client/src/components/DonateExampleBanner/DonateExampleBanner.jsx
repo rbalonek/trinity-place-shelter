623e6c6d713f90925f8fdeca6f40ca0bfcb9c093
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-24 10:35:21 -0400

    style bg

--- client/src/components/DonateExampleBanner/DonateExampleBanner.jsx
+++ client/src/components/DonateExampleBanner/DonateExampleBanner.jsx
@@ -4,7 +4,7 @@ import './DonateExampleBanner.css'
 function DonateExampleBanner() {
   return (
     <div className="donateExampleBanner">
-      <h1>DONATE EXAMPLES BANNER</h1>
+      <h1>HERE ARE EXAMPLES OF HOW YOUR GIFT WILL MAKE AN IMPACT</h1>
     </div>
   )
 }
