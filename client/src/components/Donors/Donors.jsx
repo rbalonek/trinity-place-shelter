abbb6b0f9e95aec91487d53531c9ca71334c3e45
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-08-24 13:52:16 -0400

    Follow us update

--- client/src/components/Donors/Donors.jsx
+++ client/src/components/Donors/Donors.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Donors.css'
-
+import Carousel from 'react-bootstrap/Carousel'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 export default function Donors() {
 
   return (
+    <>
     <div className="banner">
      
       <div className="container">
@@ -13,7 +16,20 @@ export default function Donors() {
           <p>Where our mission is to help homeless lesbian,gay,bisexual,trasngender or queer(LGBTQ*)youth and young adults in New York City to safety transition out of the shelter system and grow into independent,positive and productive adults.</p>
         </div>
       </div>
-    </div>
+
+      <div className="box">
+      <iframe title="vimeo-player" src="https://player.vimeo.com/video/68974299" width="300" height="360" frameborder="0" allowfullscreen></iframe></div>
+      <div className="follow-us">
+          <h2>FOLLOW US FOR CURRENT EVENT</h2>
+          <div class="social-media">
+          <a href="#" target="_blank" class="social-icon"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"  height="50"></img></a>
+          <a href="#" target="_blank" class="social-icon"><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" height="50"></img></a>
+          <a href="#" target="_blank" class="social-icon"><img src="https://w7.pngwing.com/pngs/239/740/png-transparent-twitter-logo-icon-twitter-file-logo-social-media-news.png" height="50"></img></a>
+</div>
+      </div>
+ 
+      </div>
+    </>
+
   )
 }
-// wop
