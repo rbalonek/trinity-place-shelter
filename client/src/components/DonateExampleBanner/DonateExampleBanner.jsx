3b64fa2985392528e893129f85ba6b0119bb4b71
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-24 10:27:34 -0400

    bg image

---
+++ client/src/components/DonateExampleBanner/DonateExampleBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DonateExampleBanner.css'
+
+function DonateExampleBanner() {
+  return (
+    <div className="donateExampleBanner">
+      <h1>DONATE EXAMPLES BANNER</h1>
+    </div>
+  )
+}
+
+export default DonateExampleBanner
