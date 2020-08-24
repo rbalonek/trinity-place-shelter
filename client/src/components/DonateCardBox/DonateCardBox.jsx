7b451b51b45bfa51ff501aadb0a5c47c9e26cb8a
Sebastian Duarte <Subsono33@gmail.com>
2020-08-24 18:43:55 -0400

    credit card box created

--- client/src/components/DonateCardBox/DonateCardBox.jsx
+++ client/src/components/DonateCardBox/DonateCardBox.jsx
@@ -3,12 +3,22 @@ import './DonateCardBox.css'
 
 function DonateCardBox() {
   return (
+  
     <div className="card-div">
+      <ul className="card-tab">
+      <div className="one-time-div">
+          <li className="one-time">ONE TIME</li>
+        </div>
+        <div className="recurring-div">
+        <li className="recurring">RECURRING</li>
+        </div>
+      </ul>
       <div className="card-image">
-        <img src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/1_pza0ul.png" alt="jh" />
-        <img src="../../assets/CreditCards/Discover.png" alt="hk" />
-        <img src="../../assets/CreditCards/AmEx.png" alt="hj" />
-        <img src="../../assets/CreditCards/Pypal.png" alt="jh" />
+        <img src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/1_pza0ul.png" className="credit-card" alt="visa" />
+        <img src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/2_a7yimq.png" className="credit-card" alt="hk" />
+        <img src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/14_q0evfg.png" className="credit-card" alt="hj" />
+        <img src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/22_cjqquj.png" className="credit-card" alt="jh" />
+        <img src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597945994/Trinity-Place-Shelter/HomePage/5_ydtq4z.png" className="credit-card" alt="jh" />
     </div>
     <form classNmae="card-form">
       <input className="card-input" placeholder="NAME ON CARD" />
