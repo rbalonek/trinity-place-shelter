a1a0284f0273cf868be5f1adfe76aadc3c558d2f
Robert Balonek <robertbalonek@gmail.com>
2020-08-25 12:05:27 -0400

    Linked

--- client/src/components/FollowUsVideo/FolowUsVideo.jsx
+++ client/src/components/FollowUsVideo/FolowUsVideo.jsx
@@ -9,10 +9,10 @@ export default function FolowUsVideo() {
       <h2>FOLLOW US</h2>
       <h2>FOR CURRENT EVENTS</h2>
       </div>
-    <div class="social-media">
-    <a href="https://www.instagram.com/trinitys_place/" target="_blank" class="social-icon"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"  height="50"></img></a>
-    <a href="https://www.facebook.com/trinityplaceshelter/" target="_blank" class="social-icon"><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" height="50"></img></a>
-    <a href="https://twitter.com/TPShelter" target="_blank" class="social-icon"><img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" height="50"></img></a>
+    <div className="social-media">
+    <a className="social-icon" href="https://www.instagram.com/trinitys_place/" target="_blank"  ><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"  height="50"></img></a>
+    <a href="https://www.facebook.com/trinityplaceshelter/" target="_blank" className="social-icon" ><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" height="50"></img></a>
+    <a href="https://twitter.com/TPShelter" target="_blank" className="social-icon" ><img src="https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png" height="50"></img></a>
     
         <div className="iframe">
       <iframe title="vimeo-player" src="https://player.vimeo.com/video/68974299" width="300" height="360" frameborder="0" allowfullscreen></iframe>
