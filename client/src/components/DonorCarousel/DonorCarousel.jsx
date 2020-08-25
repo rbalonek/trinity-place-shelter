561ef9677f6bccad6578533e8dffdcc9f5dddacd
Robert Balonek <robertbalonek@gmail.com>
2020-08-25 19:59:54 -0400

    Rough Draft maj doners bar

--- client/src/components/DonorCarousel/DonorCarousel.jsx
+++ client/src/components/DonorCarousel/DonorCarousel.jsx
@@ -9,23 +9,28 @@ export default function DonorCarousel() {
       <h3 className="doners-text">MAJOR DONORS</h3>
       <ChevronLeftIcon className="chevron--donors" />
       <img
+        className="img-carousel"
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1598306691/Trinity-Place-Shelter/HomePage/AidsInst_vcypsl.png"
         alt="aids-institute"
       />
       <img
+      className="img-carousel"
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1598306691/Trinity-Place-Shelter/HomePage/viacom_gziix6.png"
         alt="viacom-cbs"
       />
       <img
+      className="img-carousel"
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1598306691/Trinity-Place-Shelter/HomePage/eltonJohn_zrrmb1.png"
         alt="elton-john-aids-foundation"
       />
       <img
+      className="img-carousel"
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1598306691/Trinity-Place-Shelter/HomePage/tudor_dgayqt.png"
         alt="james-tudor-foundation"
       />
       <ChevronRightIcon className="chevron--donors" />
       <img
+        className="seal-of-transparency"
         style={{ zIndex: '100' }}
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597512463/Trinity-Place-Shelter/HomePage/Seal_of_transparancy_niagnw.png"
         alt="seal-of-transparency"
