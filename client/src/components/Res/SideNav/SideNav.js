a435bf81e1af5d66bbfdca73c45aad273cc76e28
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-25 21:11:27 -0400

    side nav placeholder

---
+++ client/src/components/Res/SideNav/SideNav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SideNav.css'
+
+function SideNav() {
+  return (
+    <div className="sideNav">
+      <h1>SIDENAV</h1>
+    </div>
+  )
+}
+
+export default SideNav
