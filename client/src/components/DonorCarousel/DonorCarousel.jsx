21ec209a49a3c9ac637383600b248e825c36c473
Barnett Lobel <lobelbarnett@gmail.com>
2020-08-26 17:16:54 -0400

    configure donor carousel component for slider functionality

--- client/src/components/DonorCarousel/DonorCarousel.jsx
+++ client/src/components/DonorCarousel/DonorCarousel.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './DonorCarousel.css'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import CarouselSlide from '../Carousel/CarouselSlide'
+// carousel images
+import pic1 from '../../assets/images/DonateCarousel/aids-institute.png'
+import pic2 from '../../assets/images/DonateCarousel/calamus.png'
+import pic3 from '../../assets/images/DonateCarousel/elton-john.png'
+import pic4 from '../../assets/images/DonateCarousel/james-tudor.png'
+import pic5 from '../../assets/images/DonateCarousel/tjx.png'
+import pic6 from '../../assets/images/DonateCarousel/van-amerigen.png'
 
 export default function DonorCarousel() {
+  const [x, setX] = useState(0)
+
+  let carouselArr = [
+    <img src={pic1} alt="pic1" />,
+    <img src={pic2} alt="pic2" />,
+    <img src={pic3} alt="pic3" />,
+    <img src={pic4} alt="pic4" />,
+    <img src={pic5} alt="pic5" />,
+    <img src={pic6} alt="pic6" />,
+  ]
+
   return (
     <div className="carousel-container">
-      <h3 className="doners-text">MAJOR DONORS</h3>
-      <ChevronLeftIcon id="left-chev" className="chevron--donors" />
-      <img
+      <div className="donors-text">
+        <h3>MAJOR DONORS</h3>
+      </div>
+      <div className="donorCarousel__leftButton">
+        <ChevronLeftIcon id="left-chev" className="chevron--donors" />
+      </div>
+      {/* carousel maps image elements onto page */}
+      <div className="donorCarousel__slider-container">
+        {carouselArr.map((image, index) => {
+          return (
+            <div
+              className="donorCarousel__slider"
+              style={{ transform: `translateX(${x}%)` }}
+            >
+              {image}
+            </div>
+          )
+        })}
+      </div>
+      <div className="donorCarousel__rightButton">
+        <ChevronRightIcon className="chevron--donors" />
+      </div>
+      {/* <img
         className="img-carousel"
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1598306691/Trinity-Place-Shelter/HomePage/AidsInst_vcypsl.png"
         alt="aids-institute"
@@ -27,9 +66,9 @@ export default function DonorCarousel() {
         className="img-carousel"
         src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1598306691/Trinity-Place-Shelter/HomePage/tudor_dgayqt.png"
         alt="james-tudor-foundation"
-      />
-      <ChevronRightIcon className="chevron--donors" />
-      <a
+      /> */}
+
+      {/* <a
         style={{ zIndex: '100' }}
         href="https://www.guidestar.org/profile/94-3447082"
         target="_blank"
@@ -41,7 +80,7 @@ export default function DonorCarousel() {
           src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1597512463/Trinity-Place-Shelter/HomePage/Seal_of_transparancy_niagnw.png"
           alt="seal-of-transparency"
         />
-      </a>
+      </a> */}
     </div>
   )
 }
